4635b8ce25547b6e0ee3e7b445796e578bd17b53
Webenvms <41431139+Webenvms@users.noreply.github.com>
2018-07-19 18:01:29 +0600

    Add files via upload

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Menu;
+use Illuminate\Http\Request;
+
+class MenuController extends Controller
+{
+    public function index()
+    {
+    	$data = Menu::find(1);
+
+    	$menus = json_decode($data->data,true);
+
+    	// return $menu;
+
+		// foreach ($menu as $character) {
+		// echo $character . '<br>';
+		// }
+
+		return view('menu.index',compact('menus'));
+
+    }
+
+
+    public function builder()
+    {
+    	$data = Menu::find(1);
+
+    	$menus = $data;
+    	return view('menu.builder',compact('menus'));
+
+    	// return $data->data;
+    }
+
+    public function update(Request $r)
+    {
+    	// return $r;
+
+    	$data = Menu::find(1);
+
+    	$data->data = $r->data;
+
+    	$data->save();
+
+    	return redirect()->back();
+
+    }
+}
